c5e0cf35f94dbfdfd402764bd19ea402d3a54d5c
showgo <45244190@qq.com>
2017-07-15 10:42:14 +0800

    update foundation.

--- sweet-foundation/src/main/java/org/sweet/frameworks/foundation/message/Messages.java
+++ sweet-foundation/src/main/java/org/sweet/frameworks/foundation/message/Messages.java
@@ -12,6 +12,8 @@ import org.sweet.frameworks.foundation.resource.PropertiesReader;
  * @modifyrecords:
  */
 public final class Messages extends PropertiesReader {
+	private static final String DEFAULT_NAMESPACE="org.sweet.frameworks.default.message";
+
 	/**
 	 * 获得系统默认消息配置字串
 	 * @param key 配置键名(短键名称,通过和Messages类的全类名拼接获得完整的配置名称后再获取配置信息)
@@ -19,7 +21,7 @@ public final class Messages extends PropertiesReader {
 	 * @return
 	 */
 	public static Object getDefault(String key,Object...parameters){
-		return get(Messages.class,key,parameters);
+		return get(DEFAULT_NAMESPACE+"."+key,parameters);
 	}
 
 	public static final String RETCODE="retcode";
