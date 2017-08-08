b768f22fe83a88bd23e09dafa8853eca28cfab0a
showgo <45244190@qq.com>
2017-08-08 16:51:14 +0800

    update base.

--- sweet-foundation/src/main/java/org/sweet/frameworks/foundation/annotation/servlet/Servlet.java
+++ sweet-foundation/src/main/java/org/sweet/frameworks/foundation/annotation/servlet/Servlet.java
@@ -2,7 +2,6 @@ package org.sweet.frameworks.foundation.annotation.servlet;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -17,7 +16,6 @@ import java.lang.annotation.Target;
  * @modifyrecords:
  */
 @Documented
-@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Servlet{
